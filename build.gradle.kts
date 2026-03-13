55baedc3c260be2e264a4dcd8f4333f02a3eb69f
chris7395328 <123130884@qq.com>
2026-03-13 18:58:45 +0800

    Add JitPack repository and update extensions-lib version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
 
@@ -9,6 +14,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
