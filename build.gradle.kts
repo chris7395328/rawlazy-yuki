2a4b9f295e97536e3bfaa52fb112f8d3b655fc77
chris7395328 <123130884@qq.com>
2026-03-13 18:16:34 +0800

    Simplify build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
-plugins {
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 allprojects {
